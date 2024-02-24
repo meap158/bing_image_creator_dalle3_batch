5bffb8508b58f059ae90b105733dcaacda242a68
HasegawaSkip <hasegawaskip@gmail.com>
2024-02-24 19:21:11 +0700

    Added setup.py for package management

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bing_image_creator',
+    version='3.8',
+    description='Batch generate images with Bing Image Creator (powered by DALL-E 3) using SeleniumBase',
+    author='Long Nguyen',
+    author_email='meapwork@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'seleniumbase',
+        'piexif',
+        'Pillow'
+    ],
+)
