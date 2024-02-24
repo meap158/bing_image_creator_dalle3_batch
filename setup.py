857bc220ffb3930d7da45b2851eae7e4a849fde6
HasegawaSkip <hasegawaskip@gmail.com>
2024-02-24 19:51:35 +0700

    Updated version to 3.9.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bing_image_creator',
-    version='3.8',
+    version='3.9',
     description='Batch generate images with Bing Image Creator (powered by DALL-E 3) using SeleniumBase',
     author='Long Nguyen',
     author_email='meapwork@gmail.com',
